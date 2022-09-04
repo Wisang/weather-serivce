a925370812bb7b5d5626d82b681bf7a7c509256b
wisang <wseom7@gmail.com>
2022-09-04 14:04:39 +0900

    query city for weather

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res) {
-  url = "https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=721fc8c69acd980dfc86a5bad72edf6f&units=metric";
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function(req, res) {
+  console.log(req.body.cityName);
+  const key = "721fc8c69acd980dfc86a5bad72edf6f";
+  const query = req.body.cityName;
+  const unit = "metric"
+
+  url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + key + "&units=" + unit;
 
   https.get(url, function(response) {
     console.log(response.statusCode);
@@ -14,12 +26,14 @@ app.get("/", function(req, res) {
       console.log(jsonData.weather[0].description);
       const weatherIcon = jsonData.weather[0].icon;
       const weatherUrl = "https://openweathermap.org/img/wn/" + weatherIcon + "@2x.png";
-      res.write("<div><h3>The weather is " + jsonData.weather[0].description + "</h3></div>");
-      res.write("<h1>The temperature now in here is " + jsonData.main.temp + " in celsius</h1>");
+      res.write("<div><h3>The weather in " + query + " is " + jsonData.weather[0].description + "</h3></div>");
+      res.write("<h1>The temperature and humidity is " + jsonData.main.temp + " in celsius and " + jsonData.main.humidity + " percent</h1>");
       res.write("<img src=" + weatherUrl + ">");
       res.send();
     });
   });
 });
+
+
 
 app.listen(3000);
